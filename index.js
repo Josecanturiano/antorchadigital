4875b3fe2dd65f0868f9b2ab7a34865dab0b385e
pj <pj>
2020-09-10 17:03:56 -0400

    Reparacion del servidor

--- index.js
+++ index.js
@@ -3,7 +3,5 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, "public")))
-  .set("views", path.join(__dirname, "views"))
-  .get("/", (req, res) => res.render("pages/index"))
+  .use(express.static("dist"))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
